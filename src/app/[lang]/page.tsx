383e9cf5d80de7bedc5acf9c77b03da307d46bf1
Dzyfhuba <hafidz21ub@gmail.com>
2024-06-09 21:41:04 +0700

    chore: Update Home component to use dictionary for dashboard title

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,17 @@
-const Home = () => {
+import { getDictionary } from "@/get-dictionary"
+import { i18n } from "@/i18n-config"
+import PageProps from "@/types/page"
+
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }))
+}
+
+const Home = async (props: PageProps) => {
+  const dictionary = await getDictionary(props.params.lang)
+
   return (
     <>
-      Home
+      <h1>{dictionary.dashboard}</h1>
     </>
   )
 }
--- src/get-dictionary.ts
+++ src/get-dictionary.ts
@@ -3,9 +3,9 @@ import type { Locale } from '@/i18n-config'
 
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
-const dictionaries: Record<string, () => Promise<{}>> = {
-  id: () => import('./dictionaries/id.json').then((module) => module.default),
+const dictionaries: Record<string, () => Promise<typeof import('./dictionaries/en.json')>> = {
   en: () => import('./dictionaries/en.json').then((module) => module.default),
+  id: () => import('./dictionaries/id.json').then((module) => module.default),
   zh: () => import('./dictionaries/zh.json').then((module) => module.default),
   ja: () => import('./dictionaries/ja.json').then((module) => module.default),
 }
